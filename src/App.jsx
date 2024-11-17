20423cd223f35d824c765917179e97a109e2f05a
Thandolwethu <thandwem045@gmail.com>
2024-11-17 08:24:24 +0200

    Cloned repo and imported Browser Router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
