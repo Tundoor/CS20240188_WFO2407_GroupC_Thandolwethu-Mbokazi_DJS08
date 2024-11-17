8e25fa05984949a1532770c478df0e6f781fe58b
Thandolwethu <thandwem045@gmail.com>
2024-11-17 19:33:10 +0200

    Updated Dashboard

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -9,6 +9,7 @@ export default function Login() {
 
     const location = useLocation()
     const navigate = useNavigate()
+    const from = location.state?.from || "/host"
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -16,8 +17,8 @@ export default function Login() {
         loginUser(loginFormData)
             .then(data => {
                 setError(null)
-                localStorage.setItem("loggedin", true)
-                navigate("/host")
+                localStorage.setItem("loggedin", false)
+                navigate("/host,", {replace: true })
             })
             .catch(err => {
                 setError(err)
