ff41dbc58c3e3c96239ab2f065302d7900bec46d
Thandolwethu <thandwem045@gmail.com>
2024-11-17 20:53:02 +0200

    Debugged HostVanDetails

--- api.js
+++ api.js
@@ -34,19 +34,15 @@ const firebaseConfig = {
     }
 
     
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+    export async function getHostVans() {
+        const q = query(vansCollectionRef, where("hostId", "==", "123"))
+        const snapshot = await getDocs(q)
+        const vans = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+        }))
+        return vans
     }
-    const data = await res.json()
-    return data.vans
-}
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
