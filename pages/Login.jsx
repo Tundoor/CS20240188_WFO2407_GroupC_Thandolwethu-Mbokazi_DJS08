783571b08e3ea66e6be60fed0d4dedae5a066ea0
Thandolwethu <thandwem045@gmail.com>
2024-11-17 18:38:49 +0200

    Updated login file

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,12 +1,29 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import {  useLocation } from "react-router-dom"
+import { loginUser }  from "../api"
 
-export default function Login() {
+
+function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
-
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+   
+    const location = useLocation()
+    
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+                console.log(data)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -19,25 +36,43 @@ export default function Login() {
 
     return (
         <div className="login-container">
-            <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className="login-form">
-                <input
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Email address"
-                    value={loginFormData.email}
-                />
-                <input
-                    name="password"
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Password"
-                    value={loginFormData.password}
-                />
-                <button>Log in</button>
-            </form>
-        </div>
+        {
+            location.state ?.message &&
+                <h3 className="login-error">{location.state.message}</h3>
+        }
+        <h1>Sign in to your account</h1>
+        {
+            error ?.message &&
+                <h3 className="login-error">{error.message}</h3>
+        }
+
+        <form onSubmit={handleSubmit} className="login-form">
+            <input
+                name="email"
+                onChange={handleChange}
+                type="email"
+                placeholder="Email address"
+                value={loginFormData.email}
+            />
+            <input
+                name="password"
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+                value={loginFormData.password}
+            />
+            <button
+                disabled={status === "submitting"}
+            >
+                {status === "submitting"
+                    ? "Logging in..."
+                    : "Log in"
+                }
+            </button>
+        </form>
+    </div>
     )
 
 }
+
+export default Login
